804bb43415e9dcc58553c7136caa399ab802d1a5
esugi <esugimot@ucsd.edu>
2018-05-10 20:52:35 -0700

    Sandwich - JSON Functionality, passable UI
    Displays JSON information in Detail page. UI is passable. Need to double check and comment.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+/*
+    TODO:
+    1. Check functionality
+    2. Make it pretty UI, List Display
+    3. Comment
+    4. Double check rubrics
+    5. Turn in
+ */
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private static final String DEFAULT_TEXT = "-";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +55,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +68,44 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich s) {
 
+        TextView tv_description = findViewById(R.id.description_tv);
+        TextView tv_alsoKnownAs = findViewById(R.id.also_known_tv);
+        TextView tv_ingredients = findViewById(R.id.ingredients_tv);
+        TextView tv_origin = findViewById(R.id.origin_tv);
+
+        if(s.getDescription().isEmpty()) {
+            tv_description.setText(DEFAULT_TEXT);
+        } else {
+            tv_description.setText(s.getDescription());
+        }
+
+        if(s.getAlsoKnownAs().isEmpty()) {
+            tv_alsoKnownAs.setText(DEFAULT_TEXT);
+        } else {
+            String aka = s.getAlsoKnownAs().toString();
+            tv_alsoKnownAs.setText(aka.substring(1,aka.length()-1));
+        }
+
+        if(s.getIngredients().isEmpty()) {
+            tv_ingredients.setText(DEFAULT_TEXT);
+        } else {
+            String ingredients = s.getIngredients().toString();
+            tv_ingredients.setText(formatListMultiLines(ingredients));
+        }
+
+        if(s.getPlaceOfOrigin().isEmpty()) {
+            tv_origin.setText(DEFAULT_TEXT);
+        } else {
+            tv_origin.setText(s.getPlaceOfOrigin());
+        }
+    }
+
+    private String formatListMultiLines(String stringList)
+    {
+        stringList = stringList.substring(1, stringList.length()-1);
+        //return stringList.replaceAll(", ", "\n");
+        return stringList;
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,58 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    private static String JSON_NAME = "name";
+    private static String JSON_MAIN_NAME = "mainName";
+    private static String JSON_AKA = "alsoKnownAs";
+    private static String JSON_ORIGIN = "placeOfOrigin";
+    private static String JSON_DESCRIPTION = "description";
+    private static String JSON_IMAGE = "image";
+    private static String JSON_INGREDIENTS = "ingredients";
+
+    public static Sandwich parseSandwichJson(String json)
+    {
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+
+            JSONObject name = jsonSandwich.optJSONObject(JSON_NAME);
+            String mainName = name.optString(JSON_MAIN_NAME);
+            List<String> alsoKnownAs = jsonArrayToList(name, JSON_AKA);
+
+            String placeOfOrigin = jsonSandwich.optString(JSON_ORIGIN);
+            String description = jsonSandwich.optString(JSON_DESCRIPTION);
+            String image = jsonSandwich.optString(JSON_IMAGE);
+            List<String> ingredients = jsonArrayToList(jsonSandwich, JSON_INGREDIENTS);
+
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // TODO: private method comment
+    private static List<String> jsonArrayToList (JSONObject object, String name)
+    {
+        List<String> list = new ArrayList<>();
+        JSONArray jsonArray = object.optJSONArray(name);
+        for (int i = 0; i < jsonArray.length(); i++)
+        {
+            list.add(jsonArray.optString(i));
+        }
+        return list;
     }
 }
